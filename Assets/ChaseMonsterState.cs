83cfc54e0f45bd128fa87e171dd82f8951303a94
Deidalus30 <paul.andre.this@gmail.com>
2023-04-18 20:55:21 +0800

    AttackState, ChaseState added
    Added attack animation and conditions

---
+++ Assets/ChaseMonsterState.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseMonsterState : StateMachineBehaviour
+{
+
+    NavMeshAgent EnemyBoss;
+    Transform player;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("player").transform;
+        EnemyBoss = animator.GetComponent<NavMeshAgent>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        EnemyBoss.destination = player.position;
+
+        if(EnemyBoss.remainingDistance <= 5)
+        {
+            animator.SetBool("IsChasing", false);
+            animator.SetBool("IsMeleeAttack", true);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -8,15 +8,28 @@ public class EnemyManager : MonoBehaviour
     public Image hpBar;
     public float maxHP;
     public float curHP;
+    public int MAttkCounter;
+
+    public ParticleSystem MeleeBoom;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         hpBar.fillAmount = curHP / maxHP;
+    }
+
+    public void MExplosion()
+    {
+        MeleeBoom.Play();
+    }
+
+    public void MeleeAttackCounter()
+    {
+        MAttkCounter++;
     }
 }
